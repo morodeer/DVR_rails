6b5baedee0abaa92b261ef89d8ec7a594397ac7d
Morodeer <morodeer@gmail.com>
2013-07-29 01:43:20 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DVRRails::Application.config.secret_key_base = '7982fd76b44631319dcf63cf304c48e6d4a06ddce8830841bc5af3bc4b8211bb436019bef6011fae8f18accb62a1b0df4da33fc89e8338071f6eeb1c6778d647'
